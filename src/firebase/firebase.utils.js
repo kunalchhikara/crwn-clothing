0a800a078d8cf344bf31288cd2072e1e3255925b
Kunal Chhikara <chhikarakunal96@gmail.com>
2020-09-07 17:56:37 +0530

    Added redux and implemented user reducer and user actions to header and app components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,56 +1,50 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { userRef } from 'react';
 
 const config = {
-    apiKey: "AIzaSyCbDM6Yc0slpNjmM7aNXdtPbsFDQiGX7cs",
-    authDomain: "crwn-clothing-project-60f8a.firebaseapp.com",
-    databaseURL: "https://crwn-clothing-project-60f8a.firebaseio.com",
-    projectId: "crwn-clothing-project-60f8a",
-    storageBucket: "crwn-clothing-project-60f8a.appspot.com",
-    messagingSenderId: "1065832227445",
-    appId: "1:1065832227445:web:6ac7f15974e316198c4e3c",
-    measurementId: "G-N1D7E6TV86"
-  };
+  apiKey: "AIzaSyCbDM6Yc0slpNjmM7aNXdtPbsFDQiGX7cs",
+  authDomain: "crwn-clothing-project-60f8a.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-project-60f8a.firebaseio.com",
+  projectId: "crwn-clothing-project-60f8a",
+  storageBucket: "crwn-clothing-project-60f8a.appspot.com",
+  messagingSenderId: "1065832227445",
+  appId: "1:1065832227445:web:6ac7f15974e316198c4e3c",
+  measurementId: "G-N1D7E6TV86"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt : 'select_account'});
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  export const signInWithGoogle = () => (
-      auth.signInWithPopup(provider)
-  );
+  const snapShot = await userRef.get();
 
-    export const createUserProfileDocument = async (userAuth, additionalData) => {
-        if (!userAuth) return;
-        
-        const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-        const snapShot = await userRef.get();
-        
-        if(!snapShot.exists) {
-          const {displayName,email} = userAuth;
-          const createdAt = new Date();
-
-          try{
-            await userRef.set({
-              displayName,
-              email,
-              createdAt,
-              ...additionalData
-            });
-          } catch (error){
-              console.log('error creating user', error.message);
-        }
-    };
-
-            return userRef;
-
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
   }
 
-  export default firebase;
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
